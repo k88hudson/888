30acd0432e997fb5a6dfb56e4f0511188f0f2210
Kate Hudson <katehudsondesign@gmail.com>
2013-03-13 15:36:55 -0400

    LIGHTHOUSE

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
-require.config({
-  context: "888",
-  baseUrl: "src",
-  paths: {
-    "text": "../lib/text",
-    "jquery": "../lib/jquery",
-    "jquery-ui": "../lib/jquery-ui",
-    "domfrag" : "../lib/textToDom"
-  }
-})([
-  "jquery", "jquery-ui", "domfrag",
-  "text!layouts/snippet.html"
-], function(
-  $, UI, Domfrag,
-  _snippet
-){
+require([
+  "jquery",
+  "text",
+  "textToDom",
+  "text!layouts/snippet.html",
+  "jquery-ui",
+  "jquery.beacon",
+  "jquery.jBeacon"
+], function( $, Text, Domfrag, _snippets ) {
+  $(function() {
 
-  // Start
-  var snippet = Domfrag( _snippet ).querySelector( ".snippet" );
+    var savedData = JSON.parse( localStorage.getItem( "BEACON_DATA" ) ) || {};
+    var addTicketInput = document.querySelector( "#add-ticket" ),
+        watchingContainer = document.querySelector( ".watching" );
 
+    $.jBeacon.settings({
+        account: "webmademovies",
+        token: "2d1dd8fea4f425542a6d006758a94421c16305d9"
+    });
+
+    function updateSavedData() {
+      localStorage.setItem( "BEACON_DATA", JSON.stringify( savedData ) );
+    }
+
+    function parseTicket( data ) {
+      var ticketData = data.ticket;
+      var el = Domfrag( _snippets ).querySelector( ".ticket" ).cloneNode( true );
+
+      $.each( ticketData, function( key, val ) {
+        var container = el.querySelector( ".t-" + key );
+        if ( container ) {
+          container.innerHTML = val;
+        }
+      });
+
+      el.querySelector( ".last-comment" ).innerHTML = data.ticket.versions[ data.ticket.version - 1 ].body_html;
+      $( watchingContainer ).prepend( el );
+    }
+
+    addTicketInput.addEventListener( "change", function( e ) {
+      console.log( this.value );
+      $.jBeacon.getTicket( 65733, this.value, parseTicket );
+    }, false );
+
+    $( watchingContainer ).sortable();
+
+  });
 });
